c336576574976e485812e8b43e02be6e372b30b3
Samuel Delesque <contact@samueldelesque.com>
2014-12-04 10:59:33 -0500

    Added handles and example usage.

--- donkeyslider.js
+++ donkeyslider.js
@@ -12,9 +12,54 @@
  *
  *  example usage: 
 	
+	//HTML
+	<div class="slider-container">
+		<div class="slider">
+			<div class="slide">
+				<img>
+				<p>Dummy text</p>
+			</div>
+			<div class="slide">
+				<img>
+				<p>Dummy text</p>
+			</div>
+			<div class="slide">
+				<img>
+				<p>Dummy text</p>
+			</div>
+		</div>
+		<div class="handles"></div>
+	</div>
+
+
+	//JS
 	$(document).ready(function() {
-		window.myDonkey = new DonkeySlider(".slider");
+		window.myDonkey = new DonkeySlider(".slider-container");
 	});
+
+	//CSS (for the handles, optional)
+	.handles{
+		margin-top:10px;
+		text-align: center;
+	.handles b{
+		display: inline-block;
+		margin: 6px;
+		width: 10px;
+		height: 10px;
+		background: #eee;
+		position: relative;
+		border-radius:10px;
+	}
+	.handles b.active:before{
+		content: "";
+		position: absolute;
+		top: 2px;
+		left: 2px;
+		width: 6px;
+		height: 6px;
+		background: #000;
+		border-radius:6px;
+	}
 
  *
  */
@@ -23,7 +68,7 @@
 	if(!$)console.error("jQuery is required for Donkey.");
 	if(!hammer)console.error("jQuery.hammer is required for Donkey.");
 	$.fn.hammer = hammer;
-	window.DonkeySlider = function(selector){
+	window.DonkeySlider = function(container,slider,slides,handles){
 		var _ = {
 			$el:null,
 			w:{},
@@ -40,21 +85,42 @@
 			//if the slider has margins
 			sliderMargin:20,
 
-			init: function(slider,slides,handles){
+			init: function(container,slider,slides,handles){
 				//construct the jQuery elements for later use
 				_.$w = $(window);
 
-				if(!slider){console.error("Please pass a valid jQuery selector to _.init()");return;}
-				_.$slider = $(slider);
+				if(!container){console.error("Please pass a valid jQuery selector to DonkeySlider()");return;}
+				_.$container = $(container);
+			
+				if(!slider){slider = ".slider"}
+				_.$slider = $(slider,_.$container);
 			
 				if(!slides){slides = ".slide"}
-				_.$slides = $(slides,_.slider);
+				_.$slides = $(slides,_.$slider);
 
 				if(!handles){handles = ".handles"}
-				_.$handles = $(handles,_.slider);
+				_.$handles = $(handles,_.$container);
+
+				_.count = _.$slides.length;
 
 				_.listen();
+				_.setStyles();
 				return _;
+			},
+
+			setStyles: function(){
+				_.$container.css({
+					overflow:"hidden",
+					width:"100%",
+					paddingRight:_.slideMargin,
+					boxSizing:"border-box",
+				});
+				_.$slides.css({
+					position:"relative",
+					marginLeft:_.slideMargin,
+					boxSizing:"border-box",
+					float:"left",
+				});
 			},
 
 			swiping: function(e){
@@ -97,27 +163,33 @@
 
 			showSlide: function(index){
 				var offset = -index*(_.slideWidth + _.slideMargin);
+				if(index == _.count-1)offset += (1-_.showPercent) * _.slideWidth;
 				_.$slider.animate({
-					transform:"translateX("+offset+"px)"
+					transform:"translateX("+Math.round(offset)+"px)"
 				});
 				_.movement = offset;
 				_.activeSlide = index;
 
-				_.$handles.find("span").removeClass("active").eq(index).addClass("active");
+				_.$handles.find("b").removeClass("active").eq(index).addClass("active");
 			},
 
 			listen: function(){
 				_.$w.resize(function(){
 					_.slideWidth = Math.round((_.$w.width()-(_.sliderMargin * 2)) * _.showPercent);
 					_.$slides.css("width",_.slideWidth);
+					_.$slider.css({
+						width: (_.slideWidth+_.slideMargin) * _.count
+					});
 				});
 				_.$w.trigger("resize");
 
 				_.$slider.hammer({drag_lock_to_axis: true}).on("release dragleft dragright swipeleft swiperight", _.swiping);
-				_.count = _.$slides.length;
 				_.$slider.trigger("release");
+
+				for(i=0;i<_.count;i++){_.$handles.append("<b></b>")}
+				_.$handles.find("b").on("click",function(e){_.showSlide($(this).index())}).eq(0).addClass("active");
 			}
 		}
-		return _.init(selector);
+		return _.init(container,slider,slides,handles);
 	}
 })(window,jQuery,jQuery.fn.hammer);
